96bd3949bf9b29223c440e89737b0ac87e3fd63d
Thomas Depierre <depierre.thomas@gmail.com>
2016-12-04 18:10:13 +0100

    Day2 solution with the 2 parts

--- lib/solutions/day2.ex
+++ lib/solutions/day2.ex
@@ -4,14 +4,11 @@ defmodule Adventofcode2016.Solution.Day2 do
     "day2.txt"
     |> Helper.load_input()
     |> walk_keypad()
-    |> IO.inspect()
   end
 
-  defp walk_keypad(input) do
+  def walk_keypad(input) do
     input
-    |> String.split("\r\n")
-    |> Enum.map(&read_movements/1)
-    |> Enum.map(&Enum.reverse/1)
+    |> generate_movement()
     |> Enum.reduce({1,1,[]}, &walk_line/2)
     |> elem(2)
     |> Enum.map(&to_tag/1)
@@ -29,6 +26,44 @@ defmodule Adventofcode2016.Solution.Day2 do
   defp walk_line([movement|tail], {x, y, eol_position}) do
     {new_x, new_y} = move(movement, x, y)
     walk_line(tail, {new_x, new_y, eol_position})
+  end
+
+  def solve_diamond() do
+    "day2.txt"
+    |> Helper.load_input()
+    |> walk_diamond()
+
+  end
+
+  def walk_diamond(input) do
+    input
+    |> generate_movement()
+    |> Enum.reduce({"5", []}, &walk_diamond_line/2)
+    |> elem(1)
+    |> Enum.reverse()
+    |> Enum.join()
+  end
+
+  @dir_to_index %{U: 0, L: 1, D: 2, R: 3}
+  @diamond_link %{"1" => "1131", "2" => "2263", "3" => "1274", "4" => "4384", "5" => "5556",
+                  "6" => "25A7", "7" => "36B8", "8" => "47C9", "9" => "9899", "A" => "6AAB",
+                  "B" => "7ADC", "C" => "8BCC", "D" => "BDDD"
+                  }
+
+  defp walk_diamond_line([], {position, eol_position}), do: {position, [position| eol_position]}
+  defp walk_diamond_line([movement|tail], {position, eol_position}) do
+    new_position =
+      @diamond_link[position]
+      |> String.at(@dir_to_index[movement])
+    walk_diamond_line(tail, {new_position, eol_position})
+  end
+
+  defp generate_movement(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.map(&read_movements/1)
   end
 
 @doc """
