cc0896dc572800092f699b59b5de2cbb3b3f867d
Thomas Depierre <depierre.thomas@gmail.com>
2016-12-05 20:35:29 +0100

    A more idiomatic helper

--- lib/solutions/helper/helper.ex
+++ lib/solutions/helper/helper.ex
@@ -1,6 +1,6 @@
 defmodule Adventofcode2016.Solution.Helper do
   def load_input(day) do
-    Path.join(:code.priv_dir(:adventofcode2016), day)
+    Path.join(Application.app_dir(:adventofcode2016, "priv"), day)
     |> File.read!()
   end
 end
