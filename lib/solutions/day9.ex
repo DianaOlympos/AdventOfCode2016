35a7f2bcb498a4518e45cf9d87766fca80bdb7e2
Thomas Depierre <depierre.thomas@gmail.com>
2016-12-09 19:37:57 +0100

    Day9 part1

---
+++ lib/solutions/day9.ex
@@ -0,0 +1,34 @@
+defmodule Adventofcode2016.Solution.Day9 do
+  alias Adventofcode2016.Solution.Helper
+
+  def solve_part1() do
+    "day9.txt"
+    |> Helper.load_input()
+    |> String.trim()
+    |> compute_length()
+    |> IO.puts
+  end
+
+  def compute_length(<<>>), do: 0
+  def compute_length(<<_a::utf8>>), do: 1
+  def compute_length("("<>rest_with_marker) do
+    {leftover, length} = parse_marker(rest_with_marker)
+    (length + compute_length(leftover))
+  end
+  def compute_length(<<_a::utf8, rest::binary>>), do: (1 + compute_length(rest))
+
+  def parse_marker(binary) do
+    {part2_to_parse, number_to_ditch} = parse_marker(binary, "")
+    {to_ditch, number_copy} = parse_marker(part2_to_parse, "")
+    size = number_to_ditch*number_copy
+    leftover = string_ditch(number_to_ditch, to_ditch)
+    {leftover, size}
+  end
+
+  def parse_marker("x"<>rest, string_int), do: {rest, String.to_integer(string_int)}
+  def parse_marker(")"<>rest, string_int), do: {rest, String.to_integer(string_int)}
+  def parse_marker(<<a::utf8, rest::binary>>, string_int), do: parse_marker(rest, string_int<><<a>>)
+
+  def string_ditch(0, rest), do: rest
+  def string_ditch(int, <<_a::utf8, rest::binary>>), do: string_ditch(int-1, rest)
+end
